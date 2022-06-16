f1b9a37c4a3d0a237c9adddbd7133506f391900e
Alexey <virtualexus@gmail.com>
2022-06-17 01:42:12 +0300

    bcrypt password encoder

--- web/src/main/java/com/epam/esm/config/UserManagement.java
+++ web/src/main/java/com/epam/esm/config/UserManagement.java
@@ -21,6 +21,6 @@ public class UserManagement {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder(10);
     }
 }
